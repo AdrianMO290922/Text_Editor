6a1cb39c0cd58c1d498f39fffcfa03c4c741491c
AdrianMO290922 <adrian.martinez.260821@gmail.com>
2025-03-20 09:48:50 -0600

    Acabado

--- editor.js
+++ editor.js
@@ -1,15 +1,19 @@
 function toBold(){
     let texto = document.getElementById("textArea");
     texto.style.fontWeight = "bold";
+    console.log("funcionaBold");
 }
 const toIncreaseSize = ()=>{
     let texto = document.getElementById("textArea");
-    let size = parseInt(texto.style.fontSize) + 2;
+    let size = parseInt(window.getComputedStyle(texto).fontSize);
+    size += 2;
     texto.style.fontSize = size + "px";
+    console.log("funcionaSize"+size);
 }
 const toDecreaseSize = ()=>{
     let texto = document.getElementById("textArea");
-    let size = parseInt(texto.style.fontSize) - 2;
+    let size = parseInt(window.getComputedStyle(texto).fontSize);
+    size -= 2;
     texto.style.fontSize = size + "px";
 }
 const toCursive = ()=>{
